c452557d64fe222e3db66c716da33353939b71b1
tmgordo <97533592+tmgordo@users.noreply.github.com>
2022-03-01 10:37:59 -0500

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,35 @@
+
+import random
+high_score= [10]
+
+def start_game():
+    print(f"*******Welcome to the guessing game******* \nThe high score is {min(high_score)}.")
+    player = input("What's your name?  ")
+    solution = random.randrange(1, 11)
+    score = 1
+    while True:
+        try:
+            guess = int(input(f"Ok, {player} give a number between 1 and 10.   "))
+            while guess != solution:
+                if guess > solution:
+                    guess = int(input(f"Sorry {player}, that's not it. Try again! It's lower!   "))
+                    score = score + 1    
+                elif guess < solution:
+                    guess = int(input(f"Sorry {player}, that's not it. Try again! It's higher!   "))
+                    score = score + 1
+                if guess <=0:
+                    raise ValueError
+                if guess >=11:
+                    raise ValueError
+            restart = input(f"Congrats {player}! You got it right! It took you {score} tries! Would you like to play again? Y/N   ")
+            if restart.lower() == "y":
+                high_score.append(score)
+                start_game()
+            elif restart.lower() != "y":
+                high_score.append(score)
+                print(f"Thanks for playing {player}. Have a beautiful day!")
+        except ValueError:
+            print("Please enter a number 1-10")
+            continue
+            
+start_game()
