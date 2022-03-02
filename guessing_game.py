bbcfe2a4dde0072242a7979fc3bf59ad030ddcc2
tmgordo <97533592+tmgordo@users.noreply.github.com>
2022-03-02 16:35:05 -0500

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -11,16 +11,16 @@ def start_game():
         try:
             guess = int(input(f"Ok, {player} give a number between 1 and 10.   "))
             while guess != solution:
+                if guess <=0:
+                    raise ValueError
+                if guess >=11:
+                    raise ValueError
                 if guess > solution:
                     guess = int(input(f"Sorry {player}, that's not it. Try again! It's lower!   "))
                     score = score + 1    
                 elif guess < solution:
                     guess = int(input(f"Sorry {player}, that's not it. Try again! It's higher!   "))
                     score = score + 1
-                if guess <=0:
-                    raise ValueError
-                if guess >=11:
-                    raise ValueError
             restart = input(f"Congrats {player}! You got it right! It took you {score} tries! Would you like to play again? Y/N   ")
             if restart.lower() == "y":
                 high_score.append(score)
@@ -28,6 +28,7 @@ def start_game():
             elif restart.lower() != "y":
                 high_score.append(score)
                 print(f"Thanks for playing {player}. Have a beautiful day!")
+                break
         except ValueError:
             print("Please enter a number 1-10")
             continue
